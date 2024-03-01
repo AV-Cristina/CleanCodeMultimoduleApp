a5427949217e3af469967f436578d799fb354d77
Cristina Afanasyeva <cristina.afanasyeva@gmail.com>
2024-03-01 22:51:50 +0300

    Step9: Добавление Unit теста для GetUserNameUseCase без использования мока

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,3 +7,9 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    implementation("org.mockito:mockito-core:5.10.0")
+    implementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    testImplementation("junit:junit:4.13.2")
+}
